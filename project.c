b99a2d4b0a4b2a587ccf39d46c0432e3d6929e3a
cosmin-oros <oroscosmin09@gmail.com>
2023-03-29 09:59:59 +0300

    init

---
+++ project.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("---- Menu ----\nn: name\nm: last modif\na: access\nPlease enter your options\n");
+
+    printf("STANDARD INPUT: ");
+    // read
+
+    return 0;
+}
